1b64a091e7651f9a1f7324c9a3a9167bd81abb16
michael.wang <michael.wang@zkteco.com>
2020-08-04 15:45:13 +0530

    remove django to site-packages,package isolated from other python env

--- easytime/init.bat
+++ easytime/init.bat
@@ -6,7 +6,7 @@ SET INSTALL_PATH=%1
 :noparms
 
 SET PRJ_NAME=mysite
-path = %INSTALL_PATH%\Python37;%INSTALL_PATH%\Utilities\;%PATH%;
+path = %INSTALL_PATH%\Python37;%INSTALL_PATH%\Utilities\;%PATH%;%INSTALL_PATH%\zkeco_dlls
 set PYTHONPATH=%INSTALL_PATH%\Python37;%INSTALL_PATH%\Python37\Lib\site-packages;%INSTALL_PATH%;%INSTALL_PATH%\zkeco_dlls
 SET DJANGO_SETTINGS_MODULE=%PRJ_NAME%.settings
 ECHO %INSTALL_PATH%
